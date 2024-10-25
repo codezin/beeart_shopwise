1d3042c1c3ad2df0a60eb81846820319c4036e36
jason.ly240 <jason.ly@at-com.com>
2024-10-25 11:03:55 +0700

    CleverWorld

--- platform/themes/CleverWorld/partials/footer.blade.php
+++ platform/themes/CleverWorld/partials/footer.blade.php
@@ -4,10 +4,11 @@
             <div class="col-md-5">
                 <p class="text-center">
                     <a href="{{ route('public.index') }}">
-                        <img src="{{ RvMedia::getImageUrl(theme_option('logo_footer')) }}" style="max-height:140px;width:auto"></a>
+                        <img src="{{ RvMedia::getImageUrl(theme_option('logo_footer')) }}"
+                            style="max-height:140px;width:auto"></a>
                 </p>
-                <h4 class="mt-5">{{theme_option('site_title')}} </h4>
-                <div class="mt-3">   {{theme_option('address')}}
+                <h4 class="mt-5">{{ theme_option('site_title') }} </h4>
+                <div class="mt-3"> {{ theme_option('address') }}
                 </div>
             </div>
 
@@ -15,50 +16,51 @@
                 <div class="row">
                     <div class="col-md-6 mt-5 mt-lg-0">
                         <div class="title-footer">
-                            <h2>{{__("Contact")}}</h2>
+                            <h2>{{ __('Contact') }}</h2>
                         </div>
                         <div class="info-item mb-3 d-flex align-items-center">
-                            <a href="tel: {{theme_option('hotline')}}">
-                            <img src="{{base}}asset/images/icon3.png">
-                            {{theme_option('hotline')}}
+                            <a href="tel: {{ theme_option('hotline') }}">
+                                <img src="{{ base }}asset/images/icon3.png">
+                                {{ theme_option('hotline') }}
                             </a>
                         </div>
                         <div class="info-item mb-3 d-flex align-items-center">
-                            <a href="mailto: {{theme_option('email')}}">
-                            <img src="{{base}}asset/images/icon2.png">
-                            {{theme_option('email')}}
+                            <a href="mailto: {{ theme_option('email') }}">
+                                <img src="{{ base }}asset/images/icon2.png">
+                                {{ theme_option('email') }}
                             </a>
                         </div>
                         <div class="info-item mb-3">
-                            <a href="https://{{theme_option('website')}}">
-                            <img src="{{base}}asset/images/world-wide.png" style="height: 24px">
-                                {{theme_option('website')}}
+                            <a href="https://{{ theme_option('website') }}">
+                                <img src="{{ base }}asset/images/world-wide.png" style="height: 24px">
+                                {{ theme_option('website') }}
                             </a>
+                        </div>
+                        <div class="info-item mb-3">
+                            {!! Menu::renderMenuLocation('categories-menu', ['view' => 'menus.footer', 'options' => ['class' => '']]) !!}
                         </div>
                     </div>
                     <div class="col-md-6 mt-5 mt-lg-0">
                         <div class="title-footer">
-                            <h2>{{__("About Us")}}</h2>
+                            <h2>{{ __('About Us') }}</h2>
                         </div>
                         <div class="d-flex justify-content-between">
                             <div class="">
                                 {!! Menu::renderMenuLocation('footer-menu', ['view' => 'menus.footer', 'options' => ['class' => '']]) !!}
                             </div>
-                            <div class="">
-                                {!! Menu::renderMenuLocation('categories-menu', ['view' => 'menus.footer', 'options' => ['class' => '']]) !!}
-                            </div>
+
                         </div>
                     </div>
                 </div>
                 <div class="row footer-parter">
 
-                    <h4 class="text-left mb-3">{{__("Corporate membership website")}}</h4>
+                    <h4 class="text-left mb-3">{{ __('Corporate membership website') }}</h4>
                     <div class="text-left">
-                        <a href="{{theme_option("cleverbox")}}" target="_blank" class="logo_cb">
-                            <img src="{{ RvMedia::getImageUrl("Logo%20Cb.svg") }}" class="mx-2">
+                        <a href="{{ theme_option('cleverbox') }}" target="_blank" class="logo_cb">
+                            <img src="{{ RvMedia::getImageUrl('Logo%20Cb.svg') }}" class="mx-2">
                         </a>
-                        <a href="{{theme_option("peektoy")}}"  target="_blank" class="logo_peektoy">
-                            <img src="{{ RvMedia::getImageUrl("PEEKTOY LOGO.svg") }}" class="mx-2">
+                        <a href="{{ theme_option('peektoy') }}" target="_blank" class="logo_peektoy">
+                            <img src="{{ RvMedia::getImageUrl('PEEKTOY LOGO.svg') }}" class="mx-2">
                         </a>
                     </div>
 
