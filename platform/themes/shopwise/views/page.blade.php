7c7ad8f752134596e2d64d51417bb6fc99ae99e3
LyHai <hailythanh@github.com>
2025-12-14 22:33:23 +0700

    [main]

--- platform/themes/shopwise/views/page.blade.php
+++ platform/themes/shopwise/views/page.blade.php
@@ -2,13 +2,11 @@
 
 <div id="app">
     @if ($page->template === 'homepage')
-         {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 
-'ck-content'])->toHtml(), $page) !!}
+        {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 'ck-content'])->toHtml(), $page) !!}
     @else
         <div class="section">
             <div class="container">
-                {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 
-'ck-content'])->toHtml(), $page) !!}
+                {!! apply_filters(PAGE_FILTER_FRONT_PAGE_CONTENT, Html::tag('div', BaseHelper::clean($page->content), ['class' => 'ck-content'])->toHtml(), $page) !!}
             </div>
         </div>
     @endif
